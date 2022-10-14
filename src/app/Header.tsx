ab8470dbd8550bf850d82a3195ea07815d13ac38
NicoCastel <nico.castelnuovo@outlook.com>
2022-10-14 17:52:29 +0200

    Update Header.tsx

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Header: React.FC = () => {
   return (
     <header>
-      <Link className='Title' to='/' >
+      <Link className='Title' to='/mastermind-game-react-app' >
         <h1>Mastermind</h1>
       </Link>
     </header>
